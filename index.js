7f83fc2b37af6e5c83378d51c73fad58cd389bfc
dhruvdakoria <dhruvdako98@gmail.com>
2022-12-10 19:56:56 -0500

    working version

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 8000;
@@ -8,6 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Set static folder as public - frontend setup middleware
+app.use(express.static(path.join(__dirname,'public')));
+
+// Routes
 app.use('/openai', require('./routes/openaiRoutes'));
 
+// Start listing on port 8000
 app.listen(port,() => console.log(`Server started on port ${port}`));
